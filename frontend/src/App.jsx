feee65f3669d160d77a4fb4c88a4d162f812b363
Dhanushp007 <dhanushdhanush053@gmail.com>
2025-06-22 17:18:06 +0000

    TV003/Frontend-Setup Deleted node_modules completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import StockChart from './components/StockChart';
 import HomeMain from './components/HomeMain';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function Screens() {
   return <p>Screens page coming soon!</p>;
@@ -12,16 +14,32 @@ function Login() {
 }
 
 export default function App() {
+  const [mode, setMode] = React.useState('light');
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+  const theme = React.useMemo(() => createTheme({
+    palette: { mode },
+  }), [mode]);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomeMain />} />
-          <Route path="chart" element={<StockChart />} />
-          <Route path="screens" element={<Screens />} />
-          <Route path="login" element={<Login />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout mode={mode} toggleColorMode={colorMode.toggleColorMode} /> }>
+            <Route index element={<HomeMain />} />
+            <Route path="chart" element={<StockChart />} />
+            <Route path="screens" element={<Screens />} />
+            <Route path="login" element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
